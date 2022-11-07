8bc3df4a48d1274160809f3058d9346e5e19a801
tsxy2007 <z.tk@163.com>
2022-11-07 22:45:08 +0800

    继续完善array

--- LearnDataStruct/arrayList_03/array.h
+++ LearnDataStruct/arrayList_03/array.h
@@ -3,7 +3,7 @@
 namespace MySTL
 {
 	template<typename T,size_t Size>
-	class Array_const_iterator
+	class _Array_const_iterator
 	{
 	public:
 		using iterator_category = random_access_iterator_tag;
@@ -12,8 +12,8 @@ namespace MySTL
 		using pointer = const T*;
 		using reference = cosnt T&;
 
-		Array_const_iterator(): _Ptr(),_idx(0){}
-		Array_const_iterator(pointer _Position,size_t off = 0) : _Ptr(_Position),_idx(off){}
+		_Array_const_iterator(): _Ptr(),_idx(0){}
+		_Array_const_iterator(pointer _Position,size_t off = 0) : _Ptr(_Position),_idx(off){}
 
 		pointer operator->()const
 		{
@@ -26,44 +26,85 @@ namespace MySTL
 			return *operator->();
 		}
 
-		Array_const_iterator& operator++
+		_Array_const_iterator& operator++
 		{
 			++_idx;
 			return *this;
 		}
 
-		Array_const_iterator operator++(int)
+		_Array_const_iterator operator++(int)
 		{
-			Array_const_iterator tmp = *this;
+			_Array_const_iterator tmp = *this;
 			++* this;
 			return tmp;
 		}
 
-		Array_const_iterator& operator-()
+		_Array_const_iterator& operator-()
 		{
 			--_idx;
 			return *this;
 		}
 
-		Array_const_iterator operator--(int)
+		_Array_const_iterator operator--(int)
 		{
-			Array_const_iterator tmp = *this;
+			_Array_const_iterator tmp = *this;
 			--* this;
 			return tmp;
 		}
 
-		Array_const_iterator& operator+=(const ptrdiff_t off)
+		_Array_const_iterator& operator+=(const ptrdiff_t off)
 		{
 			_idx += static_cast<size_t>(off);
 			return *this;
 		}
 
-		Array_const_iterator& operator-(const ptrdiff_t off)
+		_Array_const_iterator& operator-(const ptrdiff_t off)
 		{
 			return *this += off;
 		}
+
+		reference operator[](const ptrdiff_t off) const
+		{
+			return *(*this + 0off);
+		}
+
+		bool operator==(const _Array_const_iterator& Right) const
+		{
+			return _idx == Right._idx;
+		}
+
+		using _Prevent_inheriting_unwrap = _Array_const_iterator;
+
+		constexpr pointer _Unwrapped() const
+		{
+			return _Ptr + _idx;
+		}
+
+		constexpr void _Verify_with(const _Array_const_iterator& Last) const
+		{
+
+		}
+
+		constexpr void _Seek_to(pointer It)
+		{
+			_idx = static_cast<size_t>(It - _Ptr);
+		}
+
+		_Array_const_iterator operator+(const ptrdiff_t Off)
+		{
+			_Array_const_iterator tmp = *this;
+			tmp += Off;
+			return tmp;
+		}
+
+		_Array_const_iterator operator-(const ptrdiff_t Off)
+		{
+			_Array_const_iterator tmp = *this;
+			tmp -= Off;
+			return tmp;
+		}
 	private:
-		void checkIndex(int theIndex) const
+		constexpr void checkIndex(int theIndex) const
 		{
 			if (_Ptr == nullptr)
 			{
@@ -82,6 +123,101 @@ namespace MySTL
 		pointer _Ptr;
 		size_t _idx;
 	};
+
+	template <class _Ty, size_t _Size>
+	class _Array_iterator : public _Array_const_iterator<_Ty, _Size>
+	{
+		using _Mybase = _Array_const_iterator<_Ty, _Size>;
+
+		using iterator_category = random_access_iterator_tag;
+		using value_type = _Ty;
+		using difference_type = ptrdiff_t;
+		using pointer = _Ty*;
+		using reference = _Ty&;
+		
+		_Array_iterator(){}
+
+		explicit _Array_iterator(pointer _Parg,size_t _off = 0 ):_Mybase(_Parg,_off){}
+
+		reference operator*() const
+		{
+			return const_cast<reference>(_Mybase::operator*());
+		}
+
+		pointer operator->() const
+		{
+			return const_cast<pointer>(_Mybase::operator->());
+		}
+
+		_Array_iterator& operator++() 
+		{
+			_Mybase::operator++();
+			return *this;
+		}
+
+		_Array_iterator operator++(int)
+		{
+			_Array_iterator _Tmp = *this;
+			_Mybase::operator++();
+			return _Tmp;
+		}
+
+		_Array_iterator& operator--()
+		{
+			_Mybase::operator--();
+			return *this;
+		}
+
+		_Array_iterator operator--(int)  
+		{
+			_Array_iterator _Tmp = *this;
+			_Mybase::operator--();
+			return _Tmp;
+		}
+
+		 _Array_iterator& operator+=(const ptrdiff_t _Off)  
+		 {
+			_Mybase::operator+=(_Off);
+			return *this;
+		}
+
+		  _Array_iterator operator+(const ptrdiff_t _Off) const  
+		  {
+			_Array_iterator _Tmp = *this;
+			_Tmp += _Off;
+			return _Tmp;
+		}
+
+		 _Array_iterator& operator-=(const ptrdiff_t _Off)  
+		 {
+			_Mybase::operator-=(_Off);
+			return *this;
+		}
+
+		using _Mybase::operator-;
+
+		_Array_iterator operator-(const ptrdiff_t _Off) const
+		{
+			_Array_iterator _Tmp = *this;
+			_Tmp -= _Off;
+			return _Tmp;
+		}
+
+		reference operator[](const ptrdiff_t _Off) const
+		{
+			return const_cast<reference>(_Mybase::operator[](_Off));
+		}
+
+		using _Prevent_inheriting_unwrap = _Array_iterator;
+
+		constexpr pointer _Unwrapped() const
+		{
+			return const_cast<pointer>(_Mybase::_Unwrapped());
+		}
+	};
+
+
+
 	template<typename T, size_t Size>
 	class array
 	{
@@ -93,6 +229,14 @@ namespace MySTL
 		using const_pointer = const T*;
 		using reference = T&;
 		using const_reference = const T&;
+
+		using iterator = _Array_iterator<_Ty, _Size>;
+		using const_iterator = _Array_const_iterator<_Ty, _Size>;
+		
+		iterator begin()
+		{
+			return iterator{};
+		}
 	private:
 		T Elem[Size];
 	};
