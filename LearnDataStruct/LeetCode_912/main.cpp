25d52d931badcd4d10a416e13a89dd43ea418dc9
tsxy2007 <z.tk@163.com>
2022-11-15 22:08:16 +0800

    堆排序

--- LearnDataStruct/LeetCode_912/main.cpp
+++ LearnDataStruct/LeetCode_912/main.cpp
@@ -67,7 +67,7 @@ std::vector<int> sortArray_03(std::vector<int>& nums)
 		int nextIndex = i + 1;
 		int exValue = nums[nextIndex];
 		int j = nextIndex-1;
-		for (; j >= 0 && exValue > nums[j]; )
+		for (; j >= 0 && exValue < nums[j]; )
 		{
 			nums[j+1] = nums[j]; 
 			j--;
@@ -204,6 +204,187 @@ std::vector<int> sortArray_06(std::vector<int>& nums)
 	return merge(newHalf1, newHalf2);
 }
 
+// 方法七: 堆排序
+void max_heapify(std::vector<int>& nums, int start)
+{
+	int end = nums.size();
+	int dad = start;
+	int son = 2 * dad + 1;
+	while (son < end)
+	{
+		if (son + 1 < end && nums[son+1] > nums[son])
+		{
+			son += 1;
+		}
+		if (nums[dad]>nums[son])
+		{
+			return;
+		}
+		else
+		{
+			swap(nums[dad], nums[son]);
+			dad = son;
+			son = 2 * dad + 1;
+		}
+	}
+}
+
+std::vector<int> sortArray_07(std::vector<int>& nums)
+{
+	int size = nums.size();
+	int i = 0;
+	for (i = size / 2 - 1; i >=0 ; i--)
+	{
+		max_heapify(nums, i);
+	}
+	std::vector<int> result;
+	while (!nums.empty())
+	{
+		int lastIndex = nums.size() - 1;
+		swap(nums[0], nums[lastIndex]);
+		result.push_back(nums[lastIndex]);
+		nums.pop_back();
+		max_heapify(nums, 0);
+	}
+	return result;
+}
+
+// 方法八: 计数排序
+std::vector<int> sortArray_08(std::vector<int>& nums)
+{
+	if (nums.size() <= 1)
+	{
+		return nums;
+	}
+	int max = nums[0];
+	int min = nums[0];
+	for (size_t i = 0; i < nums.size() ; i++)
+	{
+		if (max < nums[i])
+		{
+			max = nums[i];
+		}
+		if (min > nums[i])
+		{
+			min = nums[i];
+		}
+	}
+	int size = (max - min) + 1;
+	std::vector<int> values(size);
+	for (size_t i = 0; i < nums.size(); i++)
+	{
+		int index = nums[i] - min;
+		values[index] += 1;
+	}
+
+	int index = 0;
+	for (size_t i = 0; i < size; i++)
+	{
+		int& value = values[i];
+		while (value)
+		{
+			nums[index++] = i + min;
+			value--;
+		}
+	}
+
+	return nums;
+}
+
+// 方法九: 桶排序
+std::vector<int> sortArray_09(std::vector<int>& nums)
+{
+	if (nums.size() <= 1)
+	{
+		return nums;
+	}
+
+	int max = nums[0];
+	int min = nums[0];
+	for (size_t i = 0; i < nums.size(); i++)
+	{
+		if (max < nums[i])
+		{
+			max = nums[i];
+		}
+		if (min > nums[i])
+		{
+			min = nums[i];
+		}
+	}
+
+	int capacity = 2;
+	int capacityCout = (max - min) / capacity + 1;
+	std::vector<std::vector<int>> buckets(capacityCout);
+	for (size_t i = 0; i < nums.size(); i++)
+	{
+		int index = (nums[i] - min) / capacity;
+		buckets[index].push_back(nums[i]);
+	}
+
+	for (size_t i = 0; i < buckets.size(); i++)
+	{
+		std::cout << "第几个桶" << i << "--------------" << std::endl;
+		for (size_t j = 0; j < buckets[i].size(); j++)
+		{
+			std::cout << buckets[i][j] << " ";
+		}
+		std::cout << std::endl;
+	}
+	std::vector<int> result;
+	for (size_t i = 0; i < buckets.size(); i++)
+	{
+		std::vector<int> tmp = sortArray_03(buckets[i]);
+		for (size_t j = 0; j < tmp.size(); j++)
+		{
+			result.push_back(tmp[j]);
+		}
+	}
+
+	return result;
+}
+
+// 方法十: 基数排序
+std::vector<int> sortArray_10(std::vector<int>& nums)
+{
+	if (nums.size() <= 1)
+	{
+		return nums;
+	}
+
+	std::vector<std::vector<int>> tens(10);
+	std::vector<std::vector<int>> units(10);
+
+	for (size_t i = 0; i < nums.size(); i++)
+	{
+		int index = nums[i] % 10;
+		units[index].push_back(nums[i]);
+	}
+	std::vector<int> tmp;
+	for (size_t i = 0; i < units.size(); i++)
+	{
+		for (size_t j = 0; j < units[i].size(); j++)
+		{
+			tmp.push_back(units[i][j]);
+		}
+	}
+
+	for (size_t i = 0; i < tmp.size(); i++)
+	{
+		int index = tmp[i] / 10;
+		tens[index].push_back(tmp[i]);
+	}
+	std::vector<int> result;
+	for (size_t i = 0; i < tens.size(); i++)
+	{
+		for (size_t j = 0; j < tens[i].size(); j++)
+		{
+			result.push_back(tens[i][j]);
+		}
+	}
+
+	return result;
+}
 
 void print(const std::vector<int>& nums)
 {
@@ -216,13 +397,17 @@ void print(const std::vector<int>& nums)
 
 int main()
 {
-	std::vector<int> a{ 5,2,1,3,4,7,8,9,6 };
+	std::vector<int> a{ 51,22,13,34,45,71,76,97,68 };
 	//std::vector<int> result = sortArray_01(a);
 	//std::vector<int> result = sortArray_02(a);
 	//std::vector<int> result = sortArray_03(a);
 	//std::vector<int> result = sortArray_04(a);
 	//std::vector<int> result = sortArray_05(a);
-	std::vector<int> result = sortArray_06(a);
+	//std::vector<int> result = sortArray_06(a);
+	//std::vector<int> result = sortArray_07(a);
+	//std::vector<int> result = sortArray_08(a);
+	//std::vector<int> result = sortArray_09(a);
+	std::vector<int> result = sortArray_10(a);
 	print(result);
 	return 0;
 }
